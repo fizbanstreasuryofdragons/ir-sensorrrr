5d645cfa7a94e7f7dc9ee5a059c1bf60dc8b2630
fizbanstreasuryofdragons <167873534+fizbanstreasuryofdragons@users.noreply.github.com>
2024-06-04 13:43:54 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -4,10 +4,12 @@ let links = 93
 let vooruit = 94
 let achteruit = 95
 let freeze = 76
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
     ir_waarde = IR.IR_read()
 })
 basic.forever(function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
     if (vooruit == ir_waarde) {
         basic.showLeds(`
             . . # . .
@@ -36,8 +38,8 @@ basic.forever(function () {
             . # . . .
             . . # . .
             `)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 15)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 15)
     }
     if (rechts == ir_waarde) {
         basic.showLeds(`
@@ -47,8 +49,8 @@ basic.forever(function () {
             . . . # .
             . . # . .
             `)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 15)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 15)
     }
     if (freeze == ir_waarde) {
         basic.showLeds(`
